d3ed2c20d879987052ab66591677b8bcc46384dc
Maxim Kurbatov <max@cubly.ru>
2019-03-25 23:07:05 +0400

    Messages and commands update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const { VK } = require("vk-io");
 const LRU = require("lru");
+const fetch = require("node-fetch");
 
 if (!process.env.TOKEN) {
   console.log("No token has been provided");
   console.log(
-    "Please proceed to https://oauth.vk.com/authorize?client_id=6243723&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,audio,video,status,messages,offline,docs&response_type=token&v=5.80"
+    "Please proceed to https://oauth.vk.com/authorize?client_id=2685278&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,audio,video,status,messages,offline,docs&response_type=token&v=5.80"
   );
   console.log("Then set TOKEN env variable");
   process.exit(1);
 }
 
 const vk = new VK();
+const HOURS = 48;
 const walkthrough = new LRU({
   max: 256,
-  maxAge: 24 * 60 * 60 * 1000
+  maxAge: HOURS * 60 * 60 * 1000
 });
 const locks = {};
 
@@ -24,7 +26,8 @@ const STEP_STARTED = 0;
 const STEP_GREETED = 1;
 const STEP_REPEATED = 2;
 const STEP_REPEATED_AGAIN = 3;
-const STEP_REPEATED_THE_LAST_TIME = 4;
+
+const msgDeactivated = m(`Автоответчик отключён на ${HOURS}ч.`);
 
 vk.setOptions({
   token: process.env.TOKEN,
@@ -42,19 +45,40 @@ updates.use(async (context, next) => {
 });
 
 updates.hear("/cat", async context => {
-  console.log("11111");
-  await context.sendPhoto("https://cataas.com/cat");
+  try {
+    await context.sendPhoto("https://cataas.com/cat");
+  } catch (e) {
+    context.send(m("Не удалось кису отправить, ошибка какая-то"));
+  }
+});
+
+updates.hear("/dog", async context => {
+  const resp = await fetch("https://dog.ceo/api/breeds/image/random").then(r =>
+    r.json()
+  );
+  try {
+    await context.sendPhoto(resp["message"]);
+  } catch (e) {
+    context.send(m("Не удалось пёсика отправить, ошибка какая-то"));
+  }
+});
+
+updates.hear("/off", async context => {
+  walkthrough.set(context.peerId, STEP_DEACTIVATED_TEMPORARY);
+  if (!context.isOutbox) {
+    await context.send(msgDeactivated);
+    return;
+  }
+  await vk.api.messages.edit({
+    peer_id: context.peerId,
+    message: context.text + " [" + msgDeactivated + "]",
+    message_id: context.id
+  });
 });
 
 function m(text) {
   return "🔹" + ZERO_WIDTH_SPACE + text;
 }
-
-const catsPurring = [
-  "http://ronsen.org/purrfectsounds/purrs/trip.mp3",
-  "http://ronsen.org/purrfectsounds/purrs/maja.mp3",
-  "http://ronsen.org/purrfectsounds/purrs/chicken.mp3"
-];
 
 updates.setHearFallbackHandler(async context => {
   if (!context.is("message")) return;
@@ -75,51 +99,41 @@ updates.setHearFallbackHandler(async context => {
   }
   switch (stage) {
     case STEP_STARTED:
-      await Promise.all([
-        context.send(
-          m(
-            "Привет! Я не сижу в VK. Если хочешь со мной связаться, найди меня в Telegram: https://t.me/maksimkurb"
-          )
-        ),
-        context.sendPhoto("https://cataas.com/cat", {
-          message: m("Ну а пока заходишь, держи кису, чтобы не скучать 😺")
-        })
-      ]);
+      await context.send(
+        m(
+          "Привет! Я не сижу в VK. Если хочешь со мной связаться, найди меня в Telegram: https://t.me/maksimkurb"
+        )
+      );
       walkthrough.set(context.peerId, STEP_GREETED);
       break;
     case STEP_GREETED:
-      const link = catsPurring[Math.floor(Math.random() * catsPurring.length)];
-      await context.send(m("Секундочку..."));
-      await context.sendVoice(link, {
-        message: m("Мур 😽")
-      });
       await context.send(
-        m(
-          "Если не хочешь Telegram, то напиши на почту: maksimkurb1+vk@gmail.com"
-        )
+        m("Если нет Telegram, то можно написать на почту: max@cubly.ru")
       );
+      try {
+        await context.sendPhoto("https://cataas.com/cat", {
+          message: m(
+            "Кстати, а ведь все любят кошек. Я тоже их люблю. Держи котеечку. Если захочешь ещё - введи /cat"
+          )
+        });
+      } catch (err) {
+        console.error(err);
+        await context.send(
+          m(
+            `Хотел кису скинуть, но произошла какая-то ошибка (${
+              err.name
+            }). Попробуй ввести /cat попозже`
+          )
+        );
+      }
       walkthrough.set(context.peerId, STEP_REPEATED);
       break;
     case STEP_REPEATED:
-      await context.send(m("Больше ничего не скажу тебе. Я просто бот."));
+      await context.send(m("Собачку хочешь? Введи /dog"));
       walkthrough.set(context.peerId, STEP_REPEATED_AGAIN);
       break;
     case STEP_REPEATED_AGAIN:
-      await context.sendPhoto(
-        "https://memepedia.ru/wp-content/uploads/2018/08/dhlxrlww0aaqd7x.jpg",
-        {
-          message: m("Ti govorish po russki?")
-        }
-      );
-      walkthrough.set(context.peerId, STEP_REPEATED_THE_LAST_TIME);
-      break;
-    case STEP_REPEATED_THE_LAST_TIME:
-      await context.sendDocument(
-        "https://media.tenor.com/images/17ab9054835c731cc7dd0e1e4f368a5b/tenor.gif",
-        {
-          message: m("Всё, пока, я на вечеринку ботов. Вернусь завтра.")
-        }
-      );
+      await context.send(m("Контактов больше не дам."));
       walkthrough.set(context.peerId, STEP_DEACTIVATED_TEMPORARY);
       break;
   }
